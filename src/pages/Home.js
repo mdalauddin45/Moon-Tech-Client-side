17a7d490ad0934c46aef5af28c51a8531df24c11
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-28 13:45:52 +0600

    products data load

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const [data, setData] = React.useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {data.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
   );
 };
