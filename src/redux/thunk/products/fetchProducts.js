a38c2af284af02776efb4b3602009a6328464736
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-29 23:56:15 +0600

    error solved

--- src/redux/thunk/products/fetchProducts.js
+++ src/redux/thunk/products/fetchProducts.js
@@ -4,9 +4,8 @@ const loadProductData = () => {
   return async (dispatch, getState) => {
     const res = await fetch("http://localhost:5000/products");
     const data = await res.json();
-
-    if (data.data.length) {
-      dispatch(loadProduct(data.data));
+    if (data) {
+      dispatch(loadProduct(data));
     }
   };
 };
